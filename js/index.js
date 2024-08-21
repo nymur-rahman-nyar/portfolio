18b8416d5c7cde4b786d46e594fa492198eb9372
NYM <101108705+nymur-rahman-nyar@users.noreply.github.com>
2024-08-21 21:47:38 +0600

    added social button below the greetings

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+document.addEventListener("DOMContentLoaded", function() {
+    var coll = document.querySelector(".collapsible");
+    var content = document.querySelector(".content");
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+    coll.addEventListener("click", function() {
+        this.classList.toggle("active");
+
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+});
 
 
 // click animation
