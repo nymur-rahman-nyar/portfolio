75a3ac95a77929c42b673e778e728430527b76be
NYM <101108705+nymur-rahman-nyar@users.noreply.github.com>
2024-08-22 04:06:06 +0600

    added a graduation countdown

--- js/index.js
+++ js/index.js
@@ -49,3 +49,33 @@ function createConfetti() {
         confetti.remove();
     }, 5000);
 }
+
+// countdown for graduation 
+// Set the date we're counting down to
+var countDownDate = new Date("Jun 1, 2026 00:00:00").getTime();
+
+// Update the countdown every 1 second
+var countdownfunction = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the countdown date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes, and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Display the result in the element with id="clock"
+  document.getElementById("clock").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the countdown is finished, write some text
+  if (distance < 0) {
+    clearInterval(countdownfunction);
+    document.getElementById("clock").innerHTML = "EXPIRED";
+  }
+}, 1000);
