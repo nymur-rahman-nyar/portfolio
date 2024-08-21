74dd398ae0fef44cc8005fad5603043266af40da
NYM <101108705+nymur-rahman-nyar@users.noreply.github.com>
2024-08-22 03:22:08 +0600

    added a new project section which expandable cards

--- js/index.js
+++ js/index.js
@@ -1,17 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var coll = document.querySelector(".collapsible");
-    var content = document.querySelector(".content");
 
-    coll.addEventListener("click", function() {
-        this.classList.toggle("active");
-
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
-    });
-});
 
 
 // click animation
